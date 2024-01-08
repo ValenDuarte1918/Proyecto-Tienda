5e9a2910c1c6dd8ff789059eb534b8afbd2a00b9
ValenDuarte1918 <valentinduarte1@hotmail.com>
2024-01-07 22:14:02 -0300

    primer commit

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": "ecoequip_db",
+    "database": "ecoequip",
     "host": process.env.DB_HOST,
     "dialect": "mysql",
     "port": process.env.DB_PORT
